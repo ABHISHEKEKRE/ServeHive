d08e9db243a06dd79a72c0e102c6ea33db83dd1e
ABHISHEK EKRE <140340275+ABHISHEKEKRE@users.noreply.github.com>
2025-04-27 01:41:49 +0530

    Update companyRoutes.js

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -4,9 +4,6 @@ const companyController = require('../controllers/companyController');
 
 router.get('/', (req, res) => res.render('main-landing'));
 router.get('/company-login', (req, res) => res.render('company-login', { errors: [] }));
-router.post('/company-signup', companyController.companySignup);
-router.post('/company-login', companyController.companyLogin);
-router.get('/company-logout', companyController.companyLogout);
 router.get('/company-dashboard', companyController.renderCompanyDashboard);
 router.get('/company-profile', companyController.renderCompanyProfile);
 router.get('/company-settings', companyController.renderCompanySettings);
@@ -16,6 +13,7 @@ router.get('/company-payments', companyController.renderCompanyPayments);
 router.get('/company-task-management', companyController.renderCompanyTaskManagement);
 router.get('/company-notifications', companyController.renderCompanyNotifications);
 router.get('/company-support', companyController.renderCompanySupport);
+router.post('/company-profile-submit',companyController.renderCompanyProfileUpdates);
 router.post('/company-profile-submit',companyController.renderCompanyProfileUpdates);
 
 module.exports = router;
