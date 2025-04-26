0038ee476eb7f69c0e75a9345da1a051dc9d9044
ABHISHEK EKRE <140340275+ABHISHEKEKRE@users.noreply.github.com>
2025-04-27 03:00:32 +0530

    Add files via upload

---
+++ controllers/messageController.js
@@ -0,0 +1,8 @@
+const Company = require('../models/companySchema');
+const Project = require('../models/projectSchema');
+const Bid = require('../models/bidSchema');
+const Freelancer=require('../models/FreelancerSchema')
+
+exports.sendMessage = async(req,res)=>{
+
+}
