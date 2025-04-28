2802c06688505e47cb06f1f7ec56679c52d2fa92
ABHISHEK EKRE <140340275+ABHISHEKEKRE@users.noreply.github.com>
2025-04-28 05:33:32 +0530

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,6 +1,14 @@
-const express= require("express");
+const express= require('express');
 const router =express.Router();
 const messageController=require('../controllers/messageController')
+const protectRoutes =require('../middleware/protectRoutes')
 
-router.post("/send/:id",messageController.sendMessage);
+router.get("/:id",protectRoutes,messageController.getMessages)
+router.post('/send/:id',protectRoutes,(req, res, next) => {
+    // Verify CSRF token
+    if (!req.csrfToken()) {
+      return res.status(403).json({ error: 'Invalid CSRF token' });
+    }
+    next();
+  },messageController.sendMessage);
 module.exports= router;
