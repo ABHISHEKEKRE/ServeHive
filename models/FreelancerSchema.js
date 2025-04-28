dfa2f2bdee5f423398106050ee3c3abd8c6c19b0
ABHISHEK EKRE <140340275+ABHISHEKEKRE@users.noreply.github.com>
2025-04-28 05:30:23 +0530

    Update FreelancerSchema.js

--- models/FreelancerSchema.js
+++ models/FreelancerSchema.js
@@ -17,6 +17,7 @@ const freelancerSchema = new mongoose.Schema({
         match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     industry:{type:String},
+
     phone: { 
         type: Number, 
      
