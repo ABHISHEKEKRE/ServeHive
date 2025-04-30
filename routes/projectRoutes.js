f3904c90ca701bc779129a8d1280fd8c2fe8d668
ABHISHEK EKRE <140340275+ABHISHEKEKRE@users.noreply.github.com>
2025-04-30 15:27:19 +0530

    Update projectRoutes.js

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
-
+const protectRoutes = require('../middleware/protectRoutes');
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: true });
 router.get('/company-post-projects', protectRoutes, projectController.renderPostProject);
 router.post('/company-post-project', protectRoutes,projectController.postProject);
 router.get('/company-manage-projects', protectRoutes, projectController.manageProjects);
